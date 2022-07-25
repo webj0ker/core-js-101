381d7049abb38d8fc94cc1445c9a09d09e74e9a3
Stas <stanislav.shendryk@gmail.com>
2022-07-25 22:40:55 +0300

    feat: 06-promises-tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -48,8 +48,9 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  // throw new Error('Not implemented');
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +72,9 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  // throw new Error('Not implemented');
+  return Promise.race(array);
 }
 
 /**
